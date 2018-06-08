f339af3e979d43156465cb22fc2ec38f11999b38
Amimaro <amir.zahlan@gmail.com>
2018-06-08 16:06:43 -0300

    :rocket: Deploy to ghp

---
+++ precache-manifest.4982e416379ee2ffdce977e5dd05008b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "78913d5af663e1644e1b",
+    "url": "/js/chunk-vendors.78913d5a.js"
+  },
+  {
+    "revision": "ba0cf96da62528fd20be",
+    "url": "/js/app.ba0cf96d.js"
+  },
+  {
+    "revision": "1272a920d1e03ea6f4be847fc4ee01d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba0cf96da62528fd20be",
+    "url": "/css/app.b5a6ee8c.css"
+  }
+];
