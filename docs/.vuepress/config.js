4fd128e7f6a860b96375e02b9e6fc8bbf1999f74
Thayana Mamoré <thayanacmamore@gmail.com>
2019-05-20 10:33:03 -0300

    Configurando link para Repo Git e possibilidade de contribuições

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
   title: 'Vue.js Norte',
   description: 'Comunidade de desenvolvedores Vue.js do Norte do Brasil',
   themeConfig: {
+    logo: 'https://raw.githubusercontent.com/vuejs-norte/artworks/master/png/logo-256x256.png',
     nav: [
       { text: 'Início', link: '/' },
       { text: 'Comunidade', link: '/comunidade/' },
@@ -11,6 +12,23 @@ module.exports = {
       ['/', 'Início'],
       ['/comunidade/', 'Comunidade'],
       ['/links/', 'Links']
-    ]
+    ],
+    // Assumes GitHub. Can also be a full GitLab url.
+    repo: 'vuejs-norte/vuejs-norte.github.io',
+    // Customising the header label
+    repoLabel: 'Contribua!',
+
+    // Optional options for generating "Edit this page" link
+
+    // if your docs are in a different repo from your main project:
+    docsRepo: 'vuejs-norte/vuejs-norte.github.io',
+    // if your docs are not at the root of the repo:
+    docsDir: 'docs',
+    // if your docs are in a specific branch (defaults to 'master'):
+    docsBranch: 'dev',
+    // defaults to false, set to true to enable
+    editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    editLinkText: 'Ajude a atualizar essa página!'
   }
 }
