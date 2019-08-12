64f0411d1391b265ac936451fa69b758da23da6c
Gabriel R <yemolai@gmail.com>
2019-08-11 22:54:36 -0300

    melhoria de visualização em aparelho móvel

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,25 +1,29 @@
 <template>
   <q-page class="flex flex-center column">
-    <img alt="Quasar logo" src="~assets/logo-256x256.png">
+    <q-img class="logo" alt="Quasar logo" src="assets/logo-256x256.png" ratio="1"/>
     <h1 class="text-h1 title text-center">
       Vue.js Norte
     </h1>
     <p class="text-h6 subtitle text-center">
       Comunidade de desenvolvedores Vue.js do Norte do Brasil
     </p>
-    <q-btn color="primary" to="/comunidade" icon-right="arrow_right_alt" size="xl">
+    <q-btn color="primary" class="q-mt-lg" to="/comunidade" icon-right="arrow_right_alt" size="xl">
       Comunidade
     </q-btn>
   </q-page>
 </template>
 
 <style scoped>
+.logo {
+  width: 50%;
+  max-width: 256px;
+}
 </style>
 
 <script>
-import { QBtn } from 'quasar'
+import { QBtn, QImg } from 'quasar'
 export default {
   name: 'PageIndex',
-  components: { QBtn }
+  components: { QBtn, QImg }
 }
 </script>
