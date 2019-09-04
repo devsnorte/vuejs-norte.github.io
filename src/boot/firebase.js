33726c50bf092f5640d545ef230d9d015183f427
Romulo Gabriel Rodrigues <yemolai@gmail.com>
2019-09-04 14:20:42 -0300

    troca de config

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -3,16 +3,16 @@ import 'firebase/auth'
 import 'firebase/database'
 
 const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyBA8lwuawgssJcmauoOlms93JkWD58EiaM',
-  authDomain: 'vuejsnortechatexperiment.firebaseapp.com',
-  databaseURL: 'https://vuejsnortechatexperiment.firebaseio.com',
-  projectId: 'vuejsnortechatexperiment',
-  storageBucket: '',
-  messagingSenderId: '129750093617',
-  appId: '1:129750093617:web:c79406a51d5fa571'
+  apiKey: 'AIzaSyDMNW8Lt6-sC96kae2GWaab69-kz8kDbT4',
+  authDomain: 'vuejs-norte.firebaseapp.com',
+  databaseURL: 'https://vuejs-norte.firebaseio.com',
+  projectId: 'vuejs-norte',
+  storageBucket: 'vuejs-norte.appspot.com',
+  messagingSenderId: '686873595132',
+  appId: '1:686873595132:web:86c33b8725838691'
 }
 
-export default async ({ router, Vue }) => {
+export default async ({ Vue }) => {
   const app = firebase.initializeApp(FIREBASE_CONFIG)
   const auth = firebase.auth()
   const database = firebase.database()
