0f1fe9bd7410da2c65ef39232e7b3f57f820d9b9
Romulo Gabriel Rodrigues <yemolai@gmail.com>
2019-09-04 14:21:18 -0300

    remoção de chat experimental

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,24 +1,25 @@
 <template>
-  <q-page class="flex flex-center column">
-    <q-img class="logo" alt="Quasar logo" src="./statics/logo-256x256.png" ratio="1"/>
-    <h1 class="text-h1 title text-center">
-      Vue.js Norte
-    </h1>
-    <p class="text-h6 subtitle text-center">
-      Comunidade de desenvolvedores Vue.js do Norte do Brasil
-    </p>
-    <q-btn
-      no-caps
-      color="primary"
-      class="q-mt-lg"
-      to="/comunidade"
-      icon-right="arrow_right_alt"
-      size="xl"
-    >
-      Comunidade
-    </q-btn>
-    <app-chat />
-  </q-page>
+  <div>
+    <q-page class="flex flex-center column">
+      <q-img class="logo" alt="Quasar logo" src="./statics/logo-256x256.png" ratio="1"/>
+      <h1 class="text-h1 title text-center">
+        Vue.js Norte
+      </h1>
+      <p class="text-h6 subtitle text-center">
+        Comunidade de desenvolvedores Vue.js do Norte do Brasil
+      </p>
+      <q-btn
+        no-caps
+        color="primary"
+        class="q-mt-lg"
+        to="/comunidade"
+        icon-right="arrow_right_alt"
+        size="xl"
+      >
+        Comunidade
+      </q-btn>
+    </q-page>
+  </div>
 </template>
 
 <style scoped>
@@ -30,9 +31,8 @@
 
 <script>
 import { QBtn, QImg } from 'quasar'
-import AppChat from 'components/chat'
 export default {
   name: 'PageIndex',
-  components: { AppChat, QBtn, QImg }
+  components: { QBtn, QImg }
 }
 </script>
