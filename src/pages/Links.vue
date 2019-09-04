80cb5705cb6167f739d571ce2477ed9c1887ecd6
Romulo Gabriel Rodrigues <yemolai@gmail.com>
2019-09-04 15:02:10 -0300

    gerando tabelas a partir de objetos

--- src/pages/Links.vue
+++ src/pages/Links.vue
@@ -15,45 +15,15 @@
       </tr>
       </thead>
       <tbody>
-        <tr>
+        <tr v-for="(item, k) in blogs" :key="`blog-${k}`">
           <td>
-            <a href="https://blog.codecasts.com.br/arquitetura-de-projetos-vue-js-com-ddd-a2bc26817793" target="_blank" rel="noopener noreferrer">
-              Arquitetura de projetos Vue.js com DDD
+            <a :href="item.url"
+              target="_blank"
+              rel="noopener noreferrer">
+              {{ item.title }}
             </a>
           </td>
-          <td>Facilitando a manutenção, reuso e escala de projetos Vue.js com Domain Driven Design.</td>
-        </tr>
-        <tr>
-          <td>
-            <a href="http://viladosilicio.com.br" target="_blank" rel="noopener noreferrer">
-              Vila do Silício
-            </a>
-          </td>
-          <td>Blog com artigos em Português</td>
-        </tr>
-        <tr>
-          <td>
-            <a href="https://vuejs-brasil.com.br" target="_blank" rel="noopener noreferrer">
-              Vue.js Brasil
-            </a>
-          </td>
-          <td>Artigos em Português sobre Vue.js</td>
-        </tr>
-        <tr>
-          <td>
-            <a href="https://vuejsdevelopers.com" target="_blank" rel="noopener noreferrer">
-              Vue.js Developers
-            </a>
-          </td>
-          <td>Artigos em Inglês sobre Vue.js</td>
-        </tr>
-        <tr>
-          <td>
-            <a href="https://gist.github.com/vinicius73/60027329a3855345e15b2eeaa3fd588d" target="_blank" rel="noopener noreferrer">
-              Guia de estudos Javascript
-            </a>
-          </td>
-          <td>Guia de estudos com lista selecionada  de artigos e vídeos sobre Javascript</td>
+          <td>{{ item.description }}</td>
         </tr>
       </tbody>
     </q-markup-table>
@@ -68,53 +38,16 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
+        <tr v-for="(item, k) in cursos" :key="`curso-${k}`">
           <td>
-            <a href="https://www.alura.com.br/curso-online-vue-parte1" target="_blank" rel="noopener noreferrer">
-              Alura - Vue.js
+            <a
+              :href="item.url"
+              target="_blank"
+              rel="noopener noreferrer">
+              {{ item.title }}
             </a>
           </td>
-          <td>Curso de Vue na Alura</td>
-        </tr>
-        <tr>
-          <td>
-            <a href="https://www.devmedia.com.br/curso/curso-de-vue-js-como-criar-sua-primeira-aplicacao/2071" target="_blank" rel="noopener noreferrer">
-              Devmedia - Vue.js: Como criar sua primeira aplicação
-            </a>
-          </td>
-          <td>Curso de Vue.js no Devmedia</td>
-        </tr>
-        <tr>
-          <td>
-            <a href="https://www.schoolofnet.com/curso/frontend/vuejs/iniciando-com-vuejs-2-rev3/" target="_blank" rel="noopener noreferrer">
-              School of Net - Iniciando com Vue.js 2
-            </a>
-          </td>
-          <td>Curso Free de Vue.js</td>
-        </tr>
-        <tr>
-          <td>
-            <a href="https://www.udemy.com/curso-javascript-ninja/" target="_blank" rel="noopener noreferrer">
-              Udemy - Curso Javascript Ninja
-            </a>
-          </td>
-          <td>Curso do Fernando Daciuk</td>
-        </tr>
-        <tr>
-          <td>
-            <a href="https://www.udemy.com/vuejs-2-the-complete-guide/" target="_blank" rel="noopener noreferrer">
-              Udemy - Vue JS 2 - The Complete Guide (incl. Vue Router &amp; Vuex)
-            </a>
-          </td>
-          <td>Curso completo de Vue.js na Udemy</td>
-        </tr>
-        <tr>
-          <td>
-            <a href="https://www.vuemastery.com/courses" target="_blank" rel="noopener noreferrer">
-              Vue Mastery
-            </a>
-          </td>
-          <td>Cursos da introdução ao avançado(Introdução é free)</td>
+          <td>{{ item.description }}</td>
         </tr>
       </tbody>
     </q-markup-table>
@@ -129,81 +62,17 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Acai Valley</td>
-          <td><a href="https://twitter.com/acaivalley" target="_blank" rel="noopener noreferrer">Twitter</a></td>
-        </tr>
-        <tr>
-          <td>Beljogos</td>
-          <td><a href="https://www.facebook.com/BeljogosPA/" target="_blank" rel="noopener noreferrer">Facebook</a>, <a
-              href="https://twitter.com/beljogos" target="_blank" rel="noopener noreferrer">Twitter</a>, <a
-              href="https://www.youtube.com/channel/UCa0FvmBQz0JiActsGs5r5-A" target="_blank"
-              rel="noopener noreferrer">Youtube</a></td>
-        </tr>
-        <tr>
-          <td>Django Girls Belém</td>
-          <td><a href="https://www.facebook.com/DjangoGirlsBelem/" target="_blank" rel="noopener noreferrer">Facebook</a>, <a
-              href="https://djangogirls.org/belem" target="_blank" rel="noopener noreferrer">Site</a></td>
-        </tr>
-        <tr>
-          <td>Enactus UFPA</td>
-          <td><a href="https://www.facebook.com/EnactusUFPA/" target="_blank" rel="noopener noreferrer">Facebook</a></td>
-        </tr>
-        <tr>
-          <td>freeCodeCamp Belém</td>
-          <td><a href="https://www.facebook.com/groups/free.code.camp.belem/" target="_blank"
-              rel="noopener noreferrer">Facebook</a>, <a href="https://github.com/fcc-belem" target="_blank"
-              rel="noopener noreferrer">Github</a>, <a href="https://freecodecamp.org/" target="_blank"
-              rel="noopener noreferrer">Site</a></td>
-        </tr>
-        <tr>
-          <td>GDG Belém</td>
-          <td><a href="https://www.facebook.com/GDGBelem/" target="_blank" rel="noopener noreferrer">Facebook</a>, <a
-              href="https://twitter.com/gdgbelem" target="_blank" rel="noopener noreferrer">Twitter</a>, <a
-              href="https://www.meetup.com/pt-BR/gdgbelemio/" target="_blank" rel="noopener noreferrer">Meetup</a>, <a
-              href="https://gdgbelem.github.io/home/" target="_blank" rel="noopener noreferrer">Site</a></td>
-        </tr>
-        <tr>
-          <td>Java Belém</td>
-          <td><a href="https://chat.whatsapp.com/KTfRmaqPRqJ4Oyn5jENfnn" target="_blank"
-              rel="noopener noreferrer">WhatsApp</a></td>
-        </tr>
-        <tr>
-          <td>Tá Safo!</td>
-          <td><a href="https://tasafo.org/" target="_blank" rel="noopener noreferrer">Blog</a>, <a
-              href="https://www.facebook.com/tasafo.comunidade/" target="_blank" rel="noopener noreferrer">Facebook</a>, <a
-              href="https://twitter.com/tasafo" target="_blank" rel="noopener noreferrer">Twitter</a>, <a
-              href="https://slack.tasafo.org" target="_blank" rel="noopener noreferrer">Slack</a></td>
-        </tr>
-        <tr>
-          <td>ParaLivre</td>
-          <td><a href="https://www.paralivre.org" target="_blank" rel="noopener noreferrer">Site</a>, <a
-              href="https://t.me/paralivre" target="_blank" rel="noopener noreferrer">Telegram</a>, <a
-              href="https://twitter.com/paralivre_" target="_blank" rel="noopener noreferrer">Twitter</a></td>
-        </tr>
-        <tr>
-          <td>PyD'égua</td>
-          <td><a href="http://pydegua.python.org.br" target="_blank" rel="noopener noreferrer">Site</a>, <a
-              href="https://github.com/pydegua/site" target="_blank" rel="noopener noreferrer">Github</a></td>
-        </tr>
-        <tr>
-          <td>PyLadies Belém</td>
-          <td><a href="https://www.facebook.com/pyladiesbelem/" target="_blank" rel="noopener noreferrer">facebook</a></td>
-        </tr>
-        <tr>
-          <td>SQL Norte</td>
-          <td><a href="https://www.facebook.com/sqlnorte/" target="_blank" rel="noopener noreferrer">Facebook</a></td>
-        </tr>
-        <tr>
-          <td>Rails Girls Belém</td>
-          <td><a href="https://www.facebook.com/railsgirlsbelem/" target="_blank" rel="noopener noreferrer">Facebook</a>, <a
-              href="https://twitter.com/railsgirlsbelem" target="_blank" rel="noopener noreferrer">Twitter</a></td>
-        </tr>
-        <tr>
-          <td>Vue Norte</td>
-          <td><a href="https://www.meetup.com/pt-BR/Vue-js-Norte/" target="_blank" rel="noopener noreferrer">Meetup</a>, <a
-              href="https://chat.whatsapp.com/7gXcqLOFdYaDmO7hgeGk6I" target="_blank" rel="noopener noreferrer">WhatsApp</a>,
-            <a href="https://github.com/vuejs-norte" target="_blank" rel="noopener noreferrer">Github</a></td>
+        <tr v-for="(comu, k) in comunidades" :key="`comu-${k}`">
+          <td>{{ comu.name }}</td>
+          <td>
+            <a
+              v-for="service in Object.keys(comu.links)"
+              :key="`comu-${k}-${service}`"
+              :href="comu.links[service]"
+              target="_blank" rel="noopener noreferrer">
+              {{ service }}
+            </a>
+          </td>
         </tr>
       </tbody>
     </q-markup-table>
@@ -218,40 +87,16 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td><a href="https://www.youtube.com/playlist?list=PLBjvYfV_TvwJlOctQ49KiOrxrFwJGqAdr" target="_blank"
-              rel="noopener noreferrer">Curso Quasar Framework - Patrick Monteiro</a></td>
-          <td>Curso de Introdução ao framework Quasar</td>
-        </tr>
-        <tr>
-          <td><a href="https://www.youtube.com/watch?v=Fn6pB3sYhn4&amp;list=PLFJmwzuHdBRTN93itG0UiZpqs8ZnBSeEF"
-              target="_blank" rel="noopener noreferrer">Vuex - Fábio Vedovelli</a></td>
-          <td>PlayList sobre Vuex</td>
-        </tr>
-        <tr>
-          <td><a href="https://www.youtube.com/watch?v=FXY1UyQfSFw&amp;list=PL55RiY5tL51qxUbODJG9cgrsVd7ZHbPrt"
-              target="_blank" rel="noopener noreferrer">Vue.js + Vuetify + Firebase Project - DevMeetup</a></td>
-          <td>Playlist do Maximilian Schwarzmüller</td>
-        </tr>
-        <tr>
-          <td><a href="https://www.youtube.com/watch?v=78tNYZUS-ps" target="_blank" rel="noopener noreferrer">The Vue Tutorial
-              for 2018 - Learn Vue 2 in 65 Minutes</a></td>
-          <td>DesignCourse</td>
-        </tr>
-        <tr>
-          <td><a href="https://www.youtube.com/watch?v=5Kqyhu_eIcw" target="_blank" rel="noopener noreferrer">VuePress
-              Tutorial - Learn how to use VuePress for Documentation Site (Beginner)</a></td>
-          <td>DesignCourse</td>
-        </tr>
-        <tr>
-          <td><a href="https://www.youtube.com/watch?v=5LYrN_cAJoA&amp;list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa"
-              target="_blank" rel="noopener noreferrer">Vue JS 2 Tutorial</a></td>
-          <td>Playlist The Net Ninja com 44 aulas</td>
-        </tr>
-        <tr>
-          <td><a href="https://www.youtube.com/watch?v=6pYUzEduLyU" target="_blank" rel="noopener noreferrer">TDD com
-              Javascript</a></td>
-          <td>Playlist com vídeo aulas sobre TDD</td>
+        <tr v-for="(item, k) in videos" :key="`video-${k}`">
+          <td>
+            <a
+              :href="item.url"
+              target="_blank"
+              rel="noopener noreferrer">
+              {{ item.title }}
+            </a>
+          </td>
+          <td>{{ item.description }}</td>
         </tr>
       </tbody>
     </q-markup-table>
@@ -261,9 +106,129 @@
 <script>
 import { QMarkupTable } from 'quasar'
 
+const blogs = [
+  { description: 'Facilitando a manutenção, reuso e escala de projetos Vue.js com Domain Driven Design.', title: 'Arquitetura de projetos Vue.js com DDD', url: 'https://blog.codecasts.com.br/arquitetura-de-projetos-vue-js-com-ddd-a2bc26817793' },
+  { description: 'Blog com artigos em Português', title: 'Vila do Silício', url: 'http://viladosilicio.com.br' },
+  { description: 'Artigos em Português sobre Vue.js', title: 'Vue.js Brasil', url: 'https://vuejs-brasil.com.br' },
+  { description: 'Artigos em Inglês sobre Vue.js', title: 'Vue.js Developers', url: 'https://vuejsdevelopers.com' },
+  { description: 'Guia de estudos com lista selecionada  de artigos e vídeos sobre Javascript', title: 'Guia de estudos Javascript', url: 'https://gist.github.com/vinicius73/60027329a3855345e15b2eeaa3fd588d' }
+]
+
+const cursos = [
+  { description: 'Curso de Vue na Alura', title: 'Alura - Vue.js', url: 'https://www.alura.com.br/curso-online-vue-parte1' },
+  { description: 'Curso de Vue.js no Devmedia', title: 'Devmedia - Vue.js: Como criar sua primeira aplicação', url: 'https://www.devmedia.com.br/curso/curso-de-vue-js-como-criar-sua-primeira-aplicacao/2071' },
+  { description: 'Curso Free de Vue.js', title: 'School of Net - Iniciando com Vue.js 2', url: 'https://www.schoolofnet.com/curso/frontend/vuejs/iniciando-com-vuejs-2-rev3/' },
+  { description: 'Curso do Fernando Daciuk', title: 'Udemy - Curso Javascript Ninja', url: 'https://www.udemy.com/curso-javascript-ninja/' },
+  { description: 'Curso completo de Vue.js na Udemy', title: 'Udemy - Vue JS 2 - The Complete Guide (incl. Vue Router &amp; Vuex)', url: 'https://www.udemy.com/vuejs-2-the-complete-guide/' },
+  { description: 'Cursos da introdução ao avançado(Introdução é free)', title: 'Vue Mastery', url: 'https://www.vuemastery.com/courses' }
+]
+
+const comunidades = [
+  { name: `Acai Valley`, links: { Twitter: 'https://twitter.com/acaivalley' } },
+  {
+    name: `Beljogos`,
+    links: {
+      Facebook: 'https://www.facebook.com/BeljogosPA/',
+      Twitter: 'https://twitter.com/beljogos',
+      YouTube: 'https://www.youtube.com/channel/UCa0FvmBQz0JiActsGs5r5-A'
+    }
+  },
+  {
+    name: `Django Girls Belém`,
+    links: {
+      Facebook: 'https://www.facebook.com/DjangoGirlsBelem/',
+      Site: 'https://djangogirls.org/belem'
+    }
+  },
+  { name: `Enactus UFPA`, links: { Facebook: 'https://www.facebook.com/EnactusUFPA/' } },
+  {
+    name: `freeCodeCamp Belém`,
+    links: {
+      Facebook: 'https://www.facebook.com/groups/free.code.camp.belem/',
+      GitHub: 'https://github.com/fcc-belem',
+      Site: 'https://freecodecamp.org/'
+    }
+  },
+  {
+    name: `GDG Belém`,
+    links: {
+      Facebook: 'https://www.facebook.com/GDGBelem/',
+      Twitter: 'https://twitter.com/gdgbelem',
+      Meetup: 'https://www.meetup.com/pt-BR/gdgbelemio/',
+      Site: 'https://gdgbelem.github.io/home/'
+    }
+  },
+  { name: `Java Belém`, links: { WhatsApp: 'https://chat.whatsapp.com/KTfRmaqPRqJ4Oyn5jENfnn' } },
+  {
+    name: `Tá Safo!`,
+    links: {
+      Blog: 'https://tasafo.org/',
+      Facebook: 'https://www.facebook.com/tasafo.comunidade/',
+      Twitter: 'https://twitter.com/tasafo',
+      Slack: 'https://slack.tasafo.org'
+    }
+  },
+  {
+    name: `ParaLivre`,
+    links: {
+      'Site': 'https://www.paralivre.org',
+      'Telegram': 'https://t.me/paralivre',
+      'Twitter': 'https://twitter.com/paralivre_'
+    }
+  },
+  {
+    name: `PyD'égua`,
+    links: {
+      Site: 'http://pydegua.python.org.br',
+      GitHub: 'https://github.com/pydegua/site'
+    }
+  },
+  {
+    name: `PyLadies Belém`,
+    links: {
+      Facebook: 'https://www.facebook.com/pyladiesbelem/'
+    }
+  },
+  {
+    name: `SQL Norte`,
+    links: {
+      Facebook: 'https://www.facebook.com/sqlnorte/'
+    }
+  },
+  {
+    name: `Rails Girls Belém`,
+    links: {
+      Facebook: 'https://www.facebook.com/railsgirlsbelem/',
+      Twitter: 'https://twitter.com/railsgirlsbelem'
+    }
+  },
+  {
+    name: `Vue Norte`,
+    links: {
+      Meetup: 'https://www.meetup.com/pt-BR/Vue-js-Norte/',
+      WhatsApp: 'https://chat.whatsapp.com/7gXcqLOFdYaDmO7hgeGk6I',
+      Site: 'https://github.com/vuejs-norte',
+      Telegram: 'https://t.me/joinchat/KjIVwE5XY11h7evjXTwU0Az'
+    }
+  }
+]
+
+const videos = [
+  { title: 'Curso Quasar Framework - Patrick Monteiro', description: 'Curso de Introdução ao framework Quasar', url: 'https://www.youtube.com/playlist?list=PLBjvYfV_TvwJlOctQ49KiOrxrFwJGqAdr' },
+  { title: 'Vuex - Fábio Vedovelli', description: 'PlayList sobre Vuex', url: 'https://www.youtube.com/watch?v=Fn6pB3sYhn4&amp;list=PLFJmwzuHdBRTN93itG0UiZpqs8ZnBSeEF' },
+  { title: 'Vue.js + Vuetify + Firebase Project - DevMeetup', description: 'Playlist do Maximilian Schwarzmüller', url: 'https://www.youtube.com/watch?v=FXY1UyQfSFw&amp;list=PL55RiY5tL51qxUbODJG9cgrsVd7ZHbPrt' },
+  { title: 'The Vue Tutorial for 2018 - Learn Vue 2 in 65 Minutes', description: 'DesignCourse', url: 'https://www.youtube.com/watch?v=78tNYZUS-ps' },
+  { title: 'VuePress Tutorial - Learn how to use VuePress for Documentation Site (Beginner)', description: 'DesignCourse', url: 'https://www.youtube.com/watch?v=5Kqyhu_eIcw' },
+  { title: 'Vue JS 2 Tutorial', description: 'Playlist The Net Ninja com 44 aulas', url: 'https://www.youtube.com/watch?v=5LYrN_cAJoA&amp;list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa' },
+  { title: 'TDD com Javascript', description: 'Playlist com vídeo aulas sobre TDD', url: 'https://www.youtube.com/watch?v=6pYUzEduLyU' }
+]
+
 export default {
   name: 'PageLinks',
-  components: { QMarkupTable }
+  components: { QMarkupTable },
+  data () {
+    return { blogs, cursos, comunidades, videos }
+  }
 }
 </script>
 
