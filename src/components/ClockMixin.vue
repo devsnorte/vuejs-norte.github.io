aa6f6f706d9c95e207ccb4c6f03351bcb767afa4
Gabriel R <yemolai@gmail.com>
2019-09-08 01:14:24 -0300

    imeplementação de timer de countdown

---
+++ src/components/ClockMixin.vue
@@ -0,0 +1,28 @@
+<script>
+import moment from 'moment'
+import 'moment/locale/pt-br'
+moment.locale('pt-br')
+
+export default {
+  date: () => ({ now: new Date(), clockRef: null }),
+  mounted () {
+    this.setClock()
+  },
+  methods: {
+    async setClock () {
+      this.tick()
+    },
+    async tick () {
+      this.now = new Date()
+      this.clockRef = setTimeout(() => this.tick(), 1000)
+    },
+    timeUntil (targetDate) {
+      return moment(this.now)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Timer.vue
@@ -0,0 +1,29 @@
+<template>
+  <span>{{ timeLeft }}</span>
+</template>
+
+<script>
+import moment from 'moment'
+import 'moment/locale/pt-br'
+import ClockMixin from './ClockMixin.vue'
+
+export default {
+  name: 'Timer',
+  mixins: [ClockMixin],
+  props: {
+    target: {
+      type: Date,
+      required: true
+    }
+  },
+  computed: {
+    timeLeft () {
+      return moment(this.now).to(this.target)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
