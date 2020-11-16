4a5995eec8f71412835bfe57a035314fc811c440
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-16 23:23:59 +0000

    Deploy vuejs-norte/vuejs-norte.github.io to github.com/vuejs-norte/vuejs-norte.github.io.git:master

--- assets/js/11.8acdc372.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{283:function(t,n,e){"use strict";e.r(n);var s=e(28),i=Object(s.a)({},(function(){var t=this.$createElement,n=this._self._c||t;return n("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[n("LinkList")],1)}),[],!1,null,null,null);n.default=i.exports}}]);
