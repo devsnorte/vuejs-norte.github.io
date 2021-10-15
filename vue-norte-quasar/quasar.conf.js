d73603514c449b0ccfc558b5dd1244d2a6642400
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2021-10-15 17:21:42 -0300

    mode history active

--- vue-norte-quasar/quasar.conf.js
+++ vue-norte-quasar/quasar.conf.js
@@ -47,7 +47,7 @@ module.exports = function (/* ctx */) {
 
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
 
